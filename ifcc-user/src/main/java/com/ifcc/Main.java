5aba5dbb00d73d3aa62eb4f92be5bafd5a049873
liufengcheng <liufengcheng@applychart.com>
2024-06-16 18:34:51 +0800

    feat:简单项目

---
+++ ifcc-user/src/main/java/com/ifcc/Main.java
@@ -0,0 +1,16 @@
+package com.ifcc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author user
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(Main.class);
+    }
+}
